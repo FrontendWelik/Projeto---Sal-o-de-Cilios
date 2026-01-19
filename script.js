c032112d6b7906037cdfdf1ecd8830796429d788
welik carlos <frontendwelikcarlos@gmail.com>
2026-01-19 16:46:46 -0300

    criei uma animação no projeto

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-function toggleMenu() {
-            const menu = document.getElementById('mobile-menu');
-            if (menu.classList.contains('hidden')) {
-                menu.classList.remove('hidden');
-                menu.classList.add('flex');
-                document.body.style.overflow = 'hidden';
-            } else {
-                menu.classList.add('hidden');
-                menu.classList.remove('flex');
-                document.body.style.overflow = 'auto';
-            }
-        }
 
-        // Animação suave para links internos
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
+
+const myObserver = new IntersectionObserver( (entries) =>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach( (element) => myObserver.observe(element))
